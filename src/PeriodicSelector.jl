e79b7183604b71a70829c9279fa6dcbbb0064abf
Lyndon White <lyndon.white@invenialabs.co.uk>
2020-05-29 19:37:42 +0100

    Make PeriodicSelector invarient to start date

--- src/PeriodicSelector.jl
+++ src/PeriodicSelector.jl
@@ -2,12 +2,13 @@
 """
     PeriodicSelector(period::Period, stride::Period=Day(1), offset::Period=Day(0))
 
-Assign holdout dates by taking a set of size `stride` once per `period` starting from the
-start date + `offset`.
+Assign holdout dates by taking a set of size `stride` once per `period`.
+The offset is relative to _Sunday 1st Jan 1899, and controls when the selected section starts.
 
-For example, `PeriodicSampler(Week(1), Day(2), Day(1))` will select 2 days per week with an
-offset of 1 day resulting in the holdout dates corresponding to the second and third days of
-each week from the start date.
+For example, `PeriodicSampler(Week(1), Day(2), Day(1))` will select 2 days per week.
+With this selected periods offset by 1 day from 1st Jan 1899.
+I.e. if applied to the first two weeks of the year 1899,
+it would select 2nd, 3rd, 9th and 8th of Jan 1899
 """
 struct PeriodicSelector <: DateSelector
     period::Period
@@ -15,18 +16,16 @@ struct PeriodicSelector <: DateSelector
     offset::Period
 
     function PeriodicSelector(period::Period, stride::Period=Day(1), offset::Period=Day(0))
-
         period ≥ Day(2) || throw(DomainError(period, "period must be at least 2 Days."))
         stride ≥ Day(1) || throw(DomainError(stride, "stride must be at least 1 Day."))
-        offset ≥ Day(0) || throw(DomainError(offset, "offset cannot be negative."))
 
         if any(isa.([period, stride, offset], Ref(Hour)))
             throw(DomainError("period, stride, and offset cannot be expressed in Hours."))
         end
 
-        if Day(stride) + Day(offset) > Day(period)
+        if Day(stride) > Day(period)
             throw(ArgumentError(
-                "Cannot take a $stride stride with offset $offset within a $period period."
+                "Cannot take a $stride stride within a $period period."
             ))
         end
 
@@ -38,17 +37,32 @@ end
 function Iterators.partition(dates::StepRange{Date, Day}, s::PeriodicSelector)
     sd, ed = extrema(dates)
 
-    holdout_dates = Date[]
-    curr = sd
-    curr += s.offset
-    while curr + s.stride <= ed
-        #TODO: in future we want to remove the assumption of a 1 day interval
-        stop = curr + s.stride - step(dates)
-        push!(holdout_dates, curr:step(dates):stop...)
-        curr += s.period
-    end
+    # The order from FERC that created the US ISO's (the first of their kind) came in 1999
+    # thus seems safe to start in 1899, which has the convient feature of
+    # starting on a Sunday
+    beginning_of_time = Date(1899)
+    initial_time = beginning_of_time + s.offset + Day(1)
+    sd < initial_time && throw(DomainError(
+        sd,
+        "PeriodicSelector with offset $(s.offset) cannot be used before $(initial_time)",
+    ))
 
-    push!(holdout_dates, curr:step(dates):ed...)
+    #NOTE: you might be thinking that this process that actually checks all dates starting
+    # from year 1899 is too slow and we should do something smart with modulo arithmetic
+    # but for current decades this takes thousands of a second and even for year 9000
+    # is still is well under 1/4 second. so keeping it simple
+
+    holdout_dates = Date[]
+    #TODO: in future we want to remove the assumption of a 1 day interval
+    curr_window = initial_time:step(dates):(initial_time + s.stride - step(dates))
+    while first(curr_window) <= ed
+        # optimization: only creating holdout window if intersect not empty
+        if last(curr_window) >= sd
+            curr_holdout_window = curr_window ∩ dates
+            append!(holdout_dates, curr_holdout_window)
+        end
+        curr_window = curr_window .+ s.period
+    end
 
     return _getdatesets(dates, holdout_dates)
 end
--- test/PeriodicSelector.jl
+++ test/PeriodicSelector.jl
@@ -2,7 +2,7 @@
     st = Date(2019, 1, 1)
     ed = Date(2019, 2, 1)
     date_range = st:Day(1):ed
-    
+
     @testset "1 week period, 1 day stride" begin
 
         selector = PeriodicSelector(Week(1))
@@ -12,12 +12,10 @@
         @test sort(vcat(collect.(vcat(result...))...)) == date_range
         @test isempty(intersect(result...))
 
-        expected_holdout = [st:Week(1):ed...]
-        @test collect(result.holdout) == expected_holdout
+        @test all(isequal(Week(1)), diff(collect(result.holdout)))
     end
 
     @testset "2 week period, 5 day stride" begin
-
         selector = PeriodicSelector(Week(2), Day(5))
         result = partition(date_range, selector)
         @test sort(vcat(collect.(vcat(result...))...)) == date_range
@@ -28,12 +26,11 @@
             st + Week(2):Day(1):st + Week(2) + Day(4)...,
             st + Week(4):Day(1):st + Week(4) + Day(3)...,
         ]
-        @test collect(result.holdout) == expected_holdout
+        @test diff(collect(result.holdout)) == Day.([1, 1, 1, 1, 10, 1, 1, 1, 1])
     end
 
-    @testset "1 week period, 1 day stride, 1 day offset" begin
-
-        selector = PeriodicSelector(Week(1), Day(1), Day(1))
+    @testset "1 week period, 1 day stride, 2 day offset" begin
+        selector = PeriodicSelector(Week(1), Day(1), Day(2))
 
         result = partition(date_range, selector)
         @test sort(vcat(collect.(vcat(result...))...)) == date_range
@@ -44,20 +41,24 @@
     end
 
     @testset "BiWeekly" begin
+        st1 = Date(1900, 1, 1)
+        ed1 = Date(1900, 2, 1)
+        date_range1 = st1:Day(1):ed1
+
         selector = PeriodicSelector(Week(2), Week(1))
 
-        result = partition(date_range, selector)
+        result = partition(date_range1, selector)
         @test isempty(intersect(result...))
 
         expected_holdout = [
-            st:Day(1):st+Day(6)...,
-            st+Week(2):Day(1):st+Week(3)-Day(1)...,
-            st+Week(4):Day(1):ed...,
+            st1:Day(1):st1+Day(6)...,
+            st1+Week(2):Day(1):st1+Week(3)-Day(1)...,
+            st1+Week(4):Day(1):ed1...,
         ]
 
         expected_validation = [
-            st+Week(1):Day(1):st+Week(2)-Day(1)...,
-            st+Week(3):Day(1):st+Week(4)-Day(1)...,
+            st1+Week(1):Day(1):st1+Week(2)-Day(1)...,
+            st1+Week(3):Day(1):st1+Week(4)-Day(1)...,
         ]
 
         @test collect(result.holdout) == expected_holdout
@@ -65,27 +66,40 @@
     end
 
     @testset "Weekends as holdout" begin
-
-        selector = PeriodicSelector(Week(1), Day(2), Day(4))
+        selector = PeriodicSelector(Week(1), Day(2), Day(5))
         validation, holdout = partition(date_range, selector)
 
         @test sort(unique(dayname.(validation))) == ["Friday", "Monday", "Thursday", "Tuesday", "Wednesday"]
         @test sort(unique(dayname.(holdout))) == ["Saturday", "Sunday"]
-
     end
 
     @testset "stride, offset, period wrong domain" begin
         @test_throws DomainError PeriodicSelector(Day(0))
         @test_throws DomainError PeriodicSelector(Day(7), Day(0))
-        @test_throws DomainError PeriodicSelector(Day(7), Day(3), Day(-1))
 
         @test_throws DomainError PeriodicSelector(Hour(24))
         @test_throws DomainError PeriodicSelector(Day(7), Hour(48))
         @test_throws DomainError PeriodicSelector(Day(7), Day(3), Hour(72))
     end
 
-    @testset "errors if stride + offset > period" begin
+    @testset "errors if stride > period" begin
         @test_throws ArgumentError PeriodicSelector(Day(2), Day(3))
-        @test_throws ArgumentError PeriodicSelector(Week(1), Day(4), Day(4))
+        @test_throws ArgumentError PeriodicSelector(Week(1), Day(8))
+    end
+
+    @testset "Invarient to date_range (period $period, stride $stride)" for
+        (period, stride) in ((Week(2), Week(1)), (Day(30), Day(10)), (Day(5), Day(2)))
+
+        @testset "offset $offset" for offset in Day.((0, 1, 2, 3))
+            selector = PeriodicSelector(period, stride, offset)
+            initial_range = Date(2029, 1, 1):Day(1):Date(2030, 1, 31)
+            initial_sets = map(Set, partition(initial_range, selector))
+
+            later_range = initial_range + Day(20)  # 20 days later.
+            later_sets = map(Set, partition(later_range, selector))
+
+            @test initial_sets.holdout ∩ later_sets.validation == Set()
+            @test initial_sets.validation ∩ later_sets.holdout == Set()
+        end
     end
 end
