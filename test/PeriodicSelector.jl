98ad8f60f42282fc20bd084ef9a660782c7b7ee1
Lyndon White <lyndon.white@invenialabs.co.uk>
2020-06-04 13:07:54 +0100

    fix indent

--- test/PeriodicSelector.jl
+++ test/PeriodicSelector.jl
@@ -106,7 +106,7 @@
     @testset "Invarient to date_range (period $period, stride $stride)" for
         (period, stride) in ((Week(2), Week(1)), (Day(30), Day(10)), (Day(5), Day(2)))
 
-         @testset "offset $offset" for offset in Day.((0, 1, 2, 3))
+        @testset "offset $offset" for offset in Day.((0, 1, 2, 3))
             selector = PeriodicSelector(period, stride, offset)
             initial_range = Date(2029, 1, 1):Day(1):Date(2030, 1, 31)
             initial_sets = map(Set, partition(initial_range, selector))
